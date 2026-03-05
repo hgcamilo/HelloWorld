de5e736e49933231d612e579b8520b61309f81c5
hgcamilo <hgcamilo@minha.fag.edu.br>
2026-03-05 19:19:03 -0300

    italiano pai

---
+++ CiaoMondo.java
@@ -0,0 +1,15 @@
+public class Italiaano {
+
+	public static void main(String[] args) {
+		
+		italian();
+		
+
+	}
+	
+	public static void italian() {
+		System.out.println("Ciao Mondo!");
+	}
+	
+
+}
