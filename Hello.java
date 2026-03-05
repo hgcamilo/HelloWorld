2ce6c6f5e630c9c5c4d2775849a26d11cdc8a36f
hgcamilo <hgcamilo@minha.fag.edu.br>
2026-03-05 19:03:45 -0300

    Hello World em ingres

---
+++ Hello.java
@@ -0,0 +1,17 @@
+package fag.git;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		gringo();
+		
+
+	}
+	
+	public static void gringo() {
+		System.out.println("Hello World");
+	}
+	
+
+}
